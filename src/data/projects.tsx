4efc8df1d65a30b0d6255f1cff1cd8e8d0d5ce63
Gabriel correia <gabriel.956622@outlook.com>
2025-04-21 15:51:29 -0300

    refactor: refatora semantica dos itens dos projetos

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -29,76 +29,24 @@ export const projects: ProjectItem[] = [
     img: SpaceTourismImage,
     alt: "Space tourism Landing Page",
     description: (
-      <>
+      <p>
         Website institucional com quatro páginas, desenvolvido com React, Styled Components, Context API e React Router.
         <br /><br />
         Criado como parte de um desafio do Frontend Mentor, o projeto explora o tema de turismo espacial, com foco em uma navegação fluida, layout responsivo e boa organização de componentes.
         <br /> <br />
         🔗 Ver no Vercel
-      </>
+      </p>
 
     ),
     link: "https://space-tourism-react-ten.vercel.app",
-  },
-  {
-    title: "IP Address Tracker | Utilizando ipify e leafletjs",
-    img: IpAdressTrackerImage,
-    alt: "Ip Adress Tracker",
-    description: (
-      <>
-        Projeto feito com HTML, CSS e JavaScript puro, integrando as APIs ipify e LeafletJS.
-        <br /><br />
-        O usuário pode inserir um IP ou domínio para obter sua localização geográfica no mapa.
-        <br /><br />
-        Exercício prático voltado para consumo de API, manipulação do DOM e exibição dinâmica de dados.
-        <br /><br />
-        🔗 Ver no GitHub Pages
-      </>
-    ),
-    link: "https://gabrielth58.github.io/IP-Address-Tracker/",
-  },
-  {
-    title: "Pokedex | React",
-    img: PokedexImage,
-    alt: "Pokedex",
-    description: (
-      <>
-        Desenvolvido utilizando React, Styled Components, Context API, React
-        Router e integração com PokeAPI.
-        <br /> <br />
-        Conta com listagem paginada de Pokémons, visualização de detalhes, Filtros, alternância de tema (claro/escuro) e responsividade.
-        <br /> <br />
-        Projeto desafiador que me ajudou a aplicar boas práticas de arquitetura de componentes e organização de pastas
-        <br /><br />
-        🔗 Ver no Vercel
-      </>
-    ),
-    link: "https://gco-quest-pokedex.vercel.app",
-  },
-  {
-    title: "Profile Search | Github API",
-    img: ProfileSearchImage,
-    alt: "Profile Search Github API",
-    description: (
-      <>
-        Este projeto foi desenvolvido com HTML, CSS e JavaScript, integração com a API do GitHub, com foco na
-        Programação Orientada a Objetos (POO).
-        <br /> <br />
-        Permite buscar e exibir perfis do GitHub com base no nome de usuário.
-        <br /><br />
-        Desafio foi aplicar POO para manter o código limpo, organizado e livre de erros.
-        🔗 Ver no Github Pages
-      </>
-    ),
-    link: "https://gabrielth58.github.io/Profile-Search-Github-API/",
   },
   {
     title: "Gabriel Correia | Portfólio",
     img: PortfolioImage,
     alt: "Portfolio",
     description: (
-      <>
-        Meu portfólio pessoal, construído com TypeScript, React e Styled Components.
+      <p>
+        Meu portfólio pessoal, construído com TypeScript, React, Styled Components e AOS Animate para animações.
         <br /> <br />
         Apresenta meus principais projetos de forma clara e organizada, com foco em acessibilidade, simplicidade e responsividade.
         <br /> <br />
@@ -109,8 +57,60 @@ export const projects: ProjectItem[] = [
         carreira como Desenvolvedor.
         <br /> <br />
         🔗 Ver no Vercel
-      </>
+      </p>
     ),
     link: "https://gabrielth58.github.io/Portfolio/",
   },
+  {
+    title: "IP Address Tracker | Utilizando ipify e leafletjs",
+    img: IpAdressTrackerImage,
+    alt: "Ip Adress Tracker",
+    description: (
+      <p>
+        Projeto feito com HTML, CSS e JavaScript puro, integrando as APIs ipify e LeafletJS.
+        <br /><br />
+        O usuário pode inserir um IP ou domínio para obter sua localização geográfica no mapa.
+        <br /><br />
+        Exercício prático voltado para consumo de API, manipulação do DOM e exibição dinâmica de dados.
+        <br /><br />
+        🔗 Ver no GitHub Pages
+      </p>
+    ),
+    link: "https://gabrielth58.github.io/IP-Address-Tracker/",
+  },
+  {
+    title: "Pokedex | React",
+    img: PokedexImage,
+    alt: "Pokedex",
+    description: (
+      <p>
+        Desenvolvido utilizando React, Styled Components, Context API, React
+        Router e integração com PokeAPI.
+        <br /> <br />
+        Conta com listagem paginada de Pokémons, visualização de detalhes, Filtros, alternância de tema (claro/escuro) e responsividade.
+        <br /> <br />
+        Projeto desafiador que me ajudou a aplicar boas práticas de arquitetura de componentes e organização de pastas
+        <br /><br />
+        🔗 Ver no Vercel
+      </p>
+    ),
+    link: "https://gco-quest-pokedex.vercel.app",
+  },
+  {
+    title: "Profile Search | Github API",
+    img: ProfileSearchImage,
+    alt: "Profile Search Github API",
+    description: (
+      <p>
+        Este projeto foi desenvolvido com HTML, CSS e JavaScript, integração com a API do GitHub, com foco na
+        Programação Orientada a Objetos (POO).
+        <br /> <br />
+        Permite buscar e exibir perfis do GitHub com base no nome de usuário.
+        <br /><br />
+        Desafio foi aplicar POO para manter o código limpo, organizado e livre de erros.
+        🔗 Ver no Github Pages
+      </p>
+    ),
+    link: "https://gabrielth58.github.io/Profile-Search-Github-API/",
+  }, 
 ]
