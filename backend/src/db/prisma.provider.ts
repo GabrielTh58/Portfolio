7c1643abba71219f78dc6f1de7490605603af1d8
Gabriel <gabriel.956622@outlook.com>
2026-03-18 04:23:57 -0300

    refactor: update prisma version

--- backend/src/db/prisma.provider.ts
+++ backend/src/db/prisma.provider.ts
@@ -1,5 +1,30 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import "dotenv/config";
+import { Injectable, Logger, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from "generated/prisma/client";
 
 @Injectable()
-export class PrismaProvider extends PrismaClient{}
+export class PrismaProvider extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL!,
+    });
+
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    try {
+      await this.$connect(); 
+      await this.$queryRaw`SELECT 1`; 
+      Logger.log('Database connection established');
+    } catch (error) {
+      Logger.error('Database connection failed', error);
+      throw error;
+    }
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
