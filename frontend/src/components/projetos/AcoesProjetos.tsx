557e7cc1271245c68f55911eddcb94ecf402a68c
Gabriel <gabriel.956622@outlook.com>
2026-04-13 21:10:36 -0300

    feat: adiciona função para links abrirem em nova guia

--- frontend/src/components/projetos/AcoesProjetos.tsx
+++ frontend/src/components/projetos/AcoesProjetos.tsx
@@ -8,6 +8,11 @@ export default function Acoes({ projeto }: { projeto: Projeto }) {
     const [loading, setLoading] = useState<"preview" | "details" | null>(null)
 
     function handleNavigate(type: "preview" | "details", url: string) {
+        if (type === "preview") {
+            window.open(url, "_blank", "noopener,noreferrer")
+            return
+        }
+
         setLoading(type)
         router.push(url)
     }
@@ -20,7 +25,7 @@ export default function Acoes({ projeto }: { projeto: Projeto }) {
                     <button
                         onClick={() => handleNavigate("preview", projeto.link!)}
                         className="flex items-center justify-center gap-2 px-6 py-3 rounded-lg w-full sm:w-auto bg-blue-600 hover:bg-blue-500/80
-                        text-white text-sm font-semibold transition-all duration-300 shadow-md hover:shadow-cyan-500/30 disabled:opacity-70"
+                        text-white text-sm font-semibold transition-all duration-300 shadow-md hover:shadow-cyan-500/30 disabled:opacity-70 cursor-pointer"
                         disabled={loading !== null}
                     >
                         {loading === "preview" ? (
@@ -34,7 +39,7 @@ export default function Acoes({ projeto }: { projeto: Projeto }) {
                     {/* DETAILS */}
                     <button
                         onClick={() => handleNavigate("details", `/projeto/${projeto.id}`)}
-                        className="flex items-center justify-center px-6 py-3 rounded-lg w-full sm:w-auto border border-zinc-700 bg-zinc-900/50 text-zinc-300 text-sm font-medium hover:border-zinc-500 hover:bg-zinc-800 hover:text-white transition-all duration-300 disabled:opacity-70"
+                        className="flex items-center justify-center px-6 py-3 rounded-lg w-full sm:w-auto border border-zinc-700 bg-zinc-900/50 text-zinc-300 text-sm font-medium hover:border-zinc-500 hover:bg-zinc-800 hover:text-white transition-all duration-300 disabled:opacity-70 cursor-pointer"
                         disabled={loading !== null}
                     >
                         {loading === "details" ? (
